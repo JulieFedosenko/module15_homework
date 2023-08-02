098fa9fd45adf062be8840f6aa3a57af1f853785
Юлия Федосенко / Yulia Fedosenko <132534095+JulieFedosenko@users.noreply.github.com>
2023-08-02 14:13:00 +1100

    Update javascript.js
    added toggle

--- TASK--01/javascript.js
+++ TASK--01/javascript.js
@@ -1,16 +1,9 @@
 function changeIcone() {
     const icon = document.getElementById('myIcon');
-    if (icon.classList.contains("icon_01")) {
-        icon.classList.remove("icon_01");
-        icon.classList.add("icon_02");
-        
-    }
-
-    else {
-        icon.classList.remove("icon_02");
-        icon.classList.add("icon_01");
-    }
+        icon.classList.toggle("icon_01"); 
+        icon.classList.toggle("icon_02");
 }
+
 
 
 
